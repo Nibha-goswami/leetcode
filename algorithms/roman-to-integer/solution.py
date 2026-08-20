74c510d6bab356adf2c5065d9dfd1d913a65614c
Nibha-goswami <officialnibhagoswami@gmail.com>
2026-08-20 14:07:00 +0530

    feat: add Roman to Integer solution

--- algorithms/roman-to-integer/solution.py
+++ algorithms/roman-to-integer/solution.py
@@ -1,11 +1,24 @@
 class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        k= 0 
-        for i in range(len(nums)):
-            if nums[i]!=val:
-                nums[k]=nums[i]
-                k+=1
-
-
-        return k
+    def romanToInt(self, s: str) -> int:
+        roman_map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        n = len(s)
+        
+        for i in range(n):
+            # If the current value is smaller than the next value, subtract it
+            if i + 1 < n and roman_map[s[i]] < roman_map[s[i + 1]]:
+                total -= roman_map[s[i]]
+            else:
+                total += roman_map[s[i]]
+                
+        return total
         
